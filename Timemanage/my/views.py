e0146d84648e27eb5f05d90514d30bc14c6cf764
jackhao2018 <45641639+jackhao2018@users.noreply.github.com>
2020-09-21 22:04:40 +0800

    我的收藏接口查询有问题，待处理

--- Timemanage/my/views.py
+++ Timemanage/my/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import Collects
+from .models import Collects, Users
 from .serializer import CollectSerializer
 from django.http import JsonResponse
 
 # Create your views here.
 
-
+#todo:收藏部分有两个问题，一，查询收藏的策略时，返回的字段是超预期的。二，还没有实现正确的连表查询机制
 class CollectView(APIView):
 
     @staticmethod
@@ -14,7 +14,8 @@ class CollectView(APIView):
         """查询我收藏的策略"""
         user_id = request.GET.get('userId')
         try:
-            collect_info = Collects.objects.filter(user_id=user_id)
+            user_info = Users.objects.get(user_id=user_id)
+            collect_info = user_info.collects
             serializer = CollectSerializer(instance=collect_info, many=True)
         except Exception as e:
             return JsonResponse({'code': '-1', 'msg': 'error', 'err_msg': f'{e}'}, safe=False)
