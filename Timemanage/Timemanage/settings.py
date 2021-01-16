00293c50d01116184a580b32d0a1516c3d678977
jackhao2018 <45641639+jackhao2018@users.noreply.github.com>
2021-01-16 18:16:40 +0800

    配置https

--- Timemanage/Timemanage/settings.py
+++ Timemanage/Timemanage/settings.py
@@ -23,6 +23,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "9#rn=s4bu!*7=@7=2l2l&ot75qjc43vo)$dvd!irnd-@39lumm"
 
+
+# SECURITY安全设置 - 支持http时建议开启
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+SECURE_SSL_REDIRECT = True  # 将所有非SSL请求永久重定向到SSL
+SESSION_COOKIE_SECURE = True  # 仅通过https传输cookie
+CSRF_COOKIE_SECURE = True  # 仅通过https传输cookie
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # 严格要求使用https协议传输
+SECURE_HSTS_PRELOAD = True  # HSTS为
+SECURE_HSTS_SECONDS = 60
+SECURE_CONTENT_TYPE_NOSNIFF = True  # 防止浏览器猜测资产的内容类型
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
